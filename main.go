341889edb2790e5bf78e52ff5741aa9e6fbf9d48
Gentry Rolofson <xdarkicex@gmail.com>
2018-10-13 16:01:13 -0700

    changing server

--- main.go
+++ main.go
@@ -1,12 +1,25 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/xDarkicex/easycare_server/server"
 )
 
 func main() {
 	s := server.LoadServer()
-	http.ListenAndServe(":3333", s.Serve())
+
+	server := &http.Server{
+		Addr:           ":80",
+		Handler:        s.Serve(),
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+	err := server.ListenAndServe()
+	if err != nil {
+		fmt.Println(err)
+	}
 }
